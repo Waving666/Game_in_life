e4c494008d9458b443213bbbd9e914475f82ca0d
Waving666 <waving6wa@gmail.com>
2019-02-22 22:44:42 +0100

    initial commit

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
+<<<<<<< HEAD
 #include <stdio.h>
+=======
+#include "stdio.h"
+#include "stdlib.h"
+
+//Живая - \u25d9
+//Мёртвая - \u25cb
+
+int main(int argc, char* argv[]){
+        //Чтение линии полецень, создание переменной структуры Configuration,
+        //Вызов ф-ии Game , передавая туда переменную Conf и путь к сохранению
+        return 0;
+}
+>>>>>>> 61cf2c5829adf4fe2096ef2015ea2a15fb163d73
